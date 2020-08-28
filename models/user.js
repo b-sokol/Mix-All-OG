106a49515fd940c01d85a2c904d2c9e541beed10
sokolb <sokolb@mac.com>
2020-08-28 19:56:23 -0400

    install npm properly

--- models/user.js
+++ models/user.js
@@ -1,14 +1,16 @@
 const mongoose = require('mongoose');
 
-const userSchema = new mongoose.Schema({
+const userSchema = new mongoose.Schema(
+  {
     name: String,
     username: String,
     favorites: [cocktailsSchema],
     comments: [commentsSchema],
     photos: [photosSchema],
     avatar: String,
-    googleId: String
-}, {
-    timestamps: true
-})
-
+    googleId: String,
+  },
+  {
+    timestamps: true,
+  }
+);
